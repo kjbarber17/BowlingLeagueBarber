fe1acb17dae22a37fbcb88df15b25c09bb7a41a8
kjbarber17 <kjbarber@dmacc.edu>
2018-02-23 14:22:38 -0600

    Initial commit

---
+++ src/controller/AddTeamServlet.java
@@ -0,0 +1,43 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Team;
+
+/**
+ * Servlet implementation class AddTeamServlet
+ */
+@WebServlet("/addTeamServlet")
+public class AddTeamServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddTeamServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String teamName = request.getParameter("teamName");
+		String teamType = request.getParameter("teamType");
+		String preferredNight = request.getParameter("preferredNight");
+		
+		Team t = new Team (teamName, teamType, preferredNight);
+		TeamHelper dao = new TeamHelper();  
+		dao.insertTeam	(t);
+		
+		getServletContext().getRequestDispatcher("/addTeam.html").forward(request, response);
+	}
+
+}
